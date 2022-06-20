e24fae0a562072f7f15ad09d0455bafa6775f58d
Ravi Dhingra <ravidhingrabkn@outlook.com>
2022-06-20 17:42:47 -0400

    some CSS changes

--- BPropertyManagement/Controllers/HomeController.cs
+++ BPropertyManagement/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace BPropertyManagement.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Us:";
 
             return View();
         }
